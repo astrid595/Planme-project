f1adb9e7c134212f0fa65e9bf6dbe8b43fb77dfe
astrid595 <84007019+astrid595@users.noreply.github.com>
2024-11-20 22:26:39 +1100

    creating folders
    general folders and app.js

--- plan-app/src/App.js
+++ plan-app/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/HomePage";
+import Dashboard from "./pages/DashboardPage";
+import Login from "./pages/LoginPage";
+import Sign from "./pages/SignPage";
+import Week from "./pages/Week";
+import List from "./pages/ListPage";
+import Today from "./pages/Today";
+import Task from "./pages/AddtaskPage";
+import Add from "./pages/AddlistPage";
+import Parent from "./pages/parentList";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/SignPage" element={<Sign />} />
+      <Route path="/LoginPage" element={<Login />} />
+      <Route path="/DashboardPage" element={<Dashboard />} />
+      <Route path="/TodayPage" element={<Today />} />
+      <Route path="/WeekPage" element={<Week />} />
+      <Route path="/ListPage" element={<List />} />
+      <Route path="/TaskPage" element={<Task />} />
+      <Route path="/AddPage" element={<Add />} />
+      <Route path="/ParentPage" element={<Parent />} />
+    </Routes>
   );
 }
-
-export default App;
