a019658662e43c974e55a221d1fc4ec7db08bbb8
astrid595 <84007019+astrid595@users.noreply.github.com>
2024-11-20 22:50:09 +1100

    add page and install
    Parent list

---
+++ plan-app/src/pages/parentList.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+// import AddTask from "../components/addTask";
+import { ListDetails } from "../mockList";
+
+const ParentList = () => {
+  const [tasks, setTasks] = useState(ListDetails);
+
+  const addNewTask = (newTask) => {
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+  };
+
+//   const newTask = {
+//     id: Math.floor(Math.random() * 10000),
+//     activity,
+//     description,
+//     time,
+//   };
+
+  addNewTask(newTask);
+
+  return (
+    <div>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.activity} - {task.description} - {task.time}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ParentList;
