e8ab7790bfa322830bd13b99be870b0a55a48f13
astrid595 <84007019+astrid595@users.noreply.github.com>
2024-11-21 21:42:12 +1100

    fixed Today and tasks page
    style of tasks

--- plan-app/src/components/tasks.jsx
+++ plan-app/src/components/tasks.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "../styles/styleT.css";
+import "../styles/styleTask.css";
 
 const Tasks = ({ desc }) => {
   return (
-    <div className="task-list">
+    <div className="list-task">
       <p className="selec"></p>
 
       <p>{desc}</p>
